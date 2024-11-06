1935af2c196282c15b786cc0021e8a8bd7452e2f
Roman Danchevskyi <r.danchevskyi@gmail.com>
2024-11-06 23:11:47 +0200

    add shadcn components

---
+++ components/Video.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
+export default function Video({ url }: { url: string }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <div className="relative pt-[56.25%]">
+      {isClient && (
+        <ReactPlayer
+          className="absolute top-0 left-0"
+          url={url}
+          width="100%"
+          height="100%"
+          loop
+          controls
+          playing
+          muted
+          config={{
+            file: {
+              attributes: {
+                controlsList: "nodownload",
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
